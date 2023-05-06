23242f3b1ba68db88be1515d50aa8e982fbe1de2
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-05-06 06:52:28 -0700

    Final save with comments

--- script.js
+++ script.js
@@ -7,32 +7,32 @@ class Rider {
     describe(){
         return `${this.name} will be competing with ${this.horse}.`;
     }
-}
+} // menu class one with constructor built by name of rider and name of their horse
 
 class Barn {
     constructor(name) {
         this.name = name;
         this.riders = [];
     }
-
+// main menu class, the riders all belong to the barn. Barn just has name constructor
     addRider(rider) {
         if (rider instanceof Rider) {
-            this.riders.push(rider);
+            this.riders.push(rider); //adds the Rider information to the array created for the Barn
         } else {
             throw new Error(`You can only add an instance of Rider. Argument is not a rider: ${rider}`);
         }
     }
     describe() {
         return `Barn ${this.name} has ${this.riders.length} riders.`
-    }
+    } // describing total riders for each barn using the riders array, prints a number. 
 }
 
 class Menu {
     constructor () {
         this.barns = [];
         this.selectedBarn = null;
-    }
-
+    } //creating the menu the and the added barns will display as an array 
+// main menu prompts below
     start() {
         let selection = this.showMainMenuOptions();
         while(selection !=0) {
@@ -64,7 +64,7 @@ class Menu {
             4) Display all Barns
             5) Exit`
         );
-   }
+   } //menu within the main barn menu to create the rider
    showBarnMenuOptions(barnInfo) {
     return prompt(`
         1) Create New Rider
@@ -73,7 +73,7 @@ class Menu {
         -------------------
         ${barnInfo}
         `);
-   }
+   } //information for each menu option
    displayBarns() {
     let barnString = "";
         for(let i = 0; i < this.barns.length; i++) {
@@ -95,7 +95,7 @@ class Menu {
             description += i + ') ' + this.selectedBarn.riders[i].describe() + '\n';
         }
         let selection1 = this.showBarnMenuOptions(description);
-        switch(selection1) {
+        switch(selection1) { 
             case '1':
                 this.createRider();
                 break;
@@ -110,7 +110,7 @@ class Menu {
     if (index > -1 && index < this.barns.length) {
         this.barns.splice(index, 1);
     }
-   }
+   } //script for the rider sub menu.
    createRider() {
     let name = prompt(`Enter the Name of the New Rider:`);
     let horse = prompt(`Enter the Name of the rider's horse: `);
@@ -124,4 +124,4 @@ class Menu {
    }
 }
 let menu = new Menu();
-menu.start();
+menu.start(); // running menu options
